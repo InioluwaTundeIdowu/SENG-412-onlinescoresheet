aa72a52b05bdb7b2a2ce84308a8b28144e920a6e
Chukwuma <nwaigbochukwuma6@gmail.com>
2023-04-29 14:44:50 +0100

    createdb

---
+++ createdb.php
@@ -0,0 +1,15 @@
+<?php
+include 'connect.php';
+
+$sql = "CREATE DATABASE OnlineScoreDb";
+if($conn){
+    if (mysqli_query($conn, $sql)) {
+        echo "Database created successfully";
+    }
+    else {
+        echo "Error creating database: ".mysqli_error($conn);
+    }
+    mysqli_close($conn);
+}
+
+?>
